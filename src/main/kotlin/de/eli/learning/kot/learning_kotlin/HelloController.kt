33d36abc3465db9941efe54eede7eeb06e42c598
eli <elias.rezapur@gmail.com>
2025-08-16 17:10:38 +0200

    Replacing logback and configuring log4j2

---
+++ src/main/kotlin/de/eli/learning/kot/learning_kotlin/HelloController.kt
@@ -0,0 +1,28 @@
+package de.eli.learning.kot.learning_kotlin
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    val logger = LoggerFactory.getLogger(this::class.java)
+    @GetMapping("/hello")
+    fun hello(): String {
+
+        logger.trace("TRACE")
+        logger.debug("DEBUG")
+        logger.info("INFO")
+        logger.warn("WARN")
+        logger.error("ERROR")
+
+        logger.info("INFO {} {} ", 1, "abc")
+        try{
+            val x = 1 / 0
+        } catch (e: Throwable){
+            logger.error("ERR failed to compute x", e)
+        }
+        return "Hello World!"
+    }
+}
